14972d6fdfbd92aa6a24125d17010c3cc966f7ab
francisco-vnobre <franciscoveloso639@yahoo.com>
2024-07-15 14:02:18 -0300

    update profile

--- user_process.php
+++ user_process.php
@@ -26,6 +26,32 @@
         $userData->email = $email;
         $userData->bio = $bio;
 
+        //Upload de imagem
+        if(iseet($_FILES["images"]) && !empty($_FILES["images"]["tmp_name"])) {
+            
+            $image = $_FILES["images"];
+            $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+            $jpgArray = ["image/jpeg", "image/jpg"];
+            if(in_array($image["type"], $imageTypes)) {
+                if(in_array($image, $jpgArray)) {
+                    $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+                }
+                else {
+                    $imageFile = imagecreatefrompng($image["tmp_name"]);
+                }
+
+                $imageName = $user->imageGenerateName();
+
+                imagejpeg($imageFile, "./img/users/" . $imageName, 100);
+
+                $userData->image = $imageName;
+            }
+            else {
+                $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+            }
+        }
+
+
         $userDao->update($userData);
     }
     else if($type === "changepassword") {
